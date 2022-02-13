f898f2c871297fd0ba306b9411b2d65c46454edf
tavaresmirako <tavaresmirako23@gmail.com>
2022-02-13 01:35:08 -0300

    s

--- projeto.js
+++ projeto.js
@@ -1,7 +1,7 @@
 function Verificarentrada() {
     NomeConvidado = document.getElementById('nome').Value;
-    convidadosCristian = ['Amanda', 'Sabrina', 'Rafael', 'Jonas', 'Carol', 'Jhonatan']
-    if (ConbidadosCristian.includes(NomeConvidado)) {
+    ConvidadosCristian = ['Amanda', 'Sabrina', 'Rafael', 'Jonas', 'Carol', 'Jhonatan']
+    if (ConvidadosCristian.includes(NomeConvidado)) {
         document.getElementById('PermissaoDeEntrada').innertext = 'voçe pode entrar!'
     } else {
         document.getElementById('PermissaoDeEntrada').innerText = 'Voçê não pode Entrar !'
